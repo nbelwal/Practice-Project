e96cc8ee1ace0c1a0413f6b5dcd475f342469464
nbelwal <89782026+nbelwal@users.noreply.github.com>
2025-02-04 11:27:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"  
+  region = "ap-south-1"
 }
 
 # Define the EC2 instance
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2_instance" {
   ami             = "ami-00bb6a80f01f03502"  
   instance_type   = "t3.small"
   security_groups = ["sg-0a1ade90b668eee03"]
-  key_name        = "keypair_aws" 
+  key_name        = "keypair_aws"
 
   # Install Docker, kubectl, and Minikube using user_data
   user_data = <<-EOF
@@ -16,33 +16,31 @@ resource "aws_instance" "ec2_instance" {
     sudo apt update -y
     
     # Install Docker
-   # Add Docker's official GPG key:
-   # sudo apt-get update
     sudo apt-get install ca-certificates curl -y
-    sudo groupadd docker 
-    sudo usermod -aG docker ubuntu  
+    sudo groupadd docker
+    sudo usermod -aG docker ubuntu
     sudo apt-get install docker.io -y
-    sudo usermod -aG docker $USER 
+    sudo usermod -aG docker $USER
     
     # Install kubectl
-     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-    
-    # Install Minikube
-   # curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
-   # sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-
-    # Start Minikube
-   # sudo minikube start
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
   EOF
 
-  # Output the public IP of the EC2 instance
+  # Tags for the instance
   tags = {
     Name = "Minikube-EC2"
   }
+
+  # Prevent unnecessary recreation due to changes in user_data
+  lifecycle {
+    ignore_changes = [
+      user_data,  # Ignore changes to user_data so it doesn't trigger an instance replacement
+    ]
+  }
 }
 
-# Output the public IP of the instance
+# Output the public IP of the EC2 instance
 output "ec2_instance_public_ip" {
   value = aws_instance.ec2_instance.public_ip
 }
