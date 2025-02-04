48312b97c92db6a5c535e569ef38b8dcd59224b8
nbelwal <89782026+nbelwal@users.noreply.github.com>
2025-02-04 11:54:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 data "aws_security_group" "testsg" {
-  name = "allow_all"
+  id = "sg-0a1ade90b668eee03"
 }
 
 # Define the EC2 instance
 resource "aws_instance" "ec2_instance" {
   ami             = "ami-00bb6a80f01f03502"  
   instance_type   = "t3.small"
-  security_groups = [data.aws_security_group.testsg.id]
+  vpc_security_group_ids = [data.aws_security_group.testsg.id]
   key_name        = "keypair_aws"
 
   # Install Docker, kubectl, and Minikube using user_data
